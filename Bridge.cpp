d68281e1828525beef2e7ab5b373c48a587dd9a0
InHakJeon <ij3512@naver.com>
2025-04-18 23:16:53 +0900

    feat: BridgePattern
    브릿지 패턴은 구현부와 추상부를 분리하여 독립적으로 변형할 수 있도록 하는 패턴이다.

---
+++ Bridge.cpp
@@ -0,0 +1,52 @@
+// 브릿지 패턴은 구현부와 추상부를 분리하여 독립적으로 변형할 수 있도록 하는 패턴이다.
+#include <iostream>
+using namespace std;
+
+class Hunting_Handler {
+  public:
+    virtual void attack() = 0; // 메서드의 인터페이스를 정의합니다.
+};
+
+class Hunting_Method1 : public Hunting_Handler {
+  public:
+    void attack() override { std::cout << "할퀴기!" << std::endl; }
+};
+
+class Hunting_Method2 : public Hunting_Handler {
+  public:
+    void attack() override { std::cout << "물어뜯기!" << std::endl; }
+};
+
+class Animal {
+  protected:
+    Hunting_Handler* hunting_method;
+  public:
+    Animal(Hunting_Handler* method) : hunting_method(method) {}
+};
+
+class Cat : public Animal {
+  public:
+    Cat(Hunting_Handler* method) : Animal(method) {}
+
+    void attack() {
+        hunting_method->attack(); // 공격 메서드를 호출합니다.
+    }
+};
+
+class Dog : public Animal {
+  public:
+    Dog(Hunting_Handler *method) : Animal(method) {}
+
+    void attack() {
+        hunting_method->attack(); // 공격 메서드를 호출합니다.
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Cat cat(new Hunting_Method1());
+    cat.attack(); // 할퀴기!
+    Dog dog(new Hunting_Method2()); 
+    dog.attack(); // 물어뜯기!
+
+    return 0;
+}
