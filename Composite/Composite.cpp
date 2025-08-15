904f1850b122a984578ec938c59707c5f8fca243
InHakJeon <ij3512@naver.com>
2025-08-15 15:46:16 +0900

    build: 기존 Composite패턴 gtest로 빌드구조 변경

--- Composite/Composite.cpp
+++ Composite/Composite.cpp
@@ -47,21 +47,3 @@ class CompositeMoney : public Money {
   private:
     list<Money*> components; // Money 객체를 포함하는 리스트입니다.
 };
-
-int main(int argc, char *argv[]) {
-    Ten ten;
-    Hundred hundred;
-    Thousand thousand;
-
-    CompositeMoney compositeMoney;
-    compositeMoney.Add(&ten); // Ten 객체를 추가합니다.
-    compositeMoney.Add(&hundred); // Hundred 객체를 추가합니다.
-    compositeMoney.Add(&thousand); // Thousand 객체를 추가합니다.
-
-    cout << "Total price: " << compositeMoney.price() << endl; // 총 가격을 출력합니다.
-    cout << "Child price: " << compositeMoney.GetChild(0)->price() << endl; // 첫 번째 자식 객체의 가격을 출력합니다.
-    cout << "Child price: " << compositeMoney.GetChild(1)->price() << endl; // 두 번째 자식 객체의 가격을 출력합니다.
-    cout << "Child price: " << compositeMoney.GetChild(2)->price() << endl; // 세 번째 자식 객체의 가격을 출력합니다.
-
-    return 0;
-}
